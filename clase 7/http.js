9e7be36d6aa68ed16b21bdacce3d814fd7230381
MariaEva91 <mariaevapizzorno@gmail.com>
2019-03-11 21:43:16 -0300

    clase 7 bis 2

--- clase 7/http.js
+++ clase 7/http.js
@@ -7,21 +7,23 @@ const servidor = http.createServer((req,res)=>{
    // res.write('Mundo');
     
     //header de content-type
-    res.statusCode = 200
-    res.setHeader('Content-Type', 'text/html');
-    res.end(`<h1> hola mari!</h1>`)
+   // res.statusCode = 200
+   // res.setHeader('Content-Type', 'text/html');
+   //res.end(`<h1> hola mari!</h1>`)
     //forma indirecta de setear el header
     //por defecto el codigo de status de http en node es siempre 200 hasta que el digamos lo contrario.
-    
+
     //Para mandar un archivo HTML completo, importo el modulo fs
     fs.readFile(`${__dirname}/index.html`,(err,data)=>{
         if(err){
             console.log(err)
             res.writeHead(500,'Internal Server Error')
             res.end(err.message)
+        }else{
+            res.writeHead(200,{'content-type':'text/html'})
+            res.end(data)
         }
-        res.writeHead({'content-type':'text/html'})
-        res.end(data)
+       
     })
     
 });
