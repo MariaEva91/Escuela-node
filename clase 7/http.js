90ffe3f16216db5337bfd333f37809c0185b49d4
MariaEva91 <mariaevapizzorno@gmail.com>
2019-03-11 21:39:07 -0300

    clase 7 bis

--- clase 7/http.js
+++ clase 7/http.js
@@ -1,4 +1,5 @@
 const http = require('http');
+const fs = require('fs');
 
 const servidor = http.createServer((req,res)=>{
     //es indispensable que una conexion http termine la escritura del socket con Socket.end
@@ -12,6 +13,16 @@ const servidor = http.createServer((req,res)=>{
     //forma indirecta de setear el header
     //por defecto el codigo de status de http en node es siempre 200 hasta que el digamos lo contrario.
     
+    //Para mandar un archivo HTML completo, importo el modulo fs
+    fs.readFile(`${__dirname}/index.html`,(err,data)=>{
+        if(err){
+            console.log(err)
+            res.writeHead(500,'Internal Server Error')
+            res.end(err.message)
+        }
+        res.writeHead({'content-type':'text/html'})
+        res.end(data)
+    })
     
 });
 servidor.listen(8000);
