7ca4d72a2ce692bd5822a17857932cdb039e3d8c
MariaEva91 <mariaevapizzorno@gmail.com>
2019-03-15 21:12:52 -0300

    npm paquetes

---
+++ clase 9/index.js
@@ -0,0 +1,7 @@
+const mysql = require('mysql');
+const io = require('socket.io');
+const http = require('http');
+
+//socket.io-client : Es el paquete de npm para webSockets de clientes
+//Se puede usar CDN para esto
+
